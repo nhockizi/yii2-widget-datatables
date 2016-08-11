a76faf515bf3c1fe3534003368ba92c08a02524d
kizi nhoc <nhockizi@gmail.com>
2016-08-11 12:06:11 +0700

    update

--- DataTablesAsset.php
+++ DataTablesAsset.php
@@ -4,12 +4,12 @@ use yii\web\AssetBundle;
 
 class DataTablesAsset extends AssetBundle 
 {
-    public $sourcePath = '@bower/nhockizi-datatables'; 
+    public $sourcePath = '@bower/datatables'; 
     public $css = [
-        "library/css/jquery.dataTables.css",
+        "media/css/jquery.dataTables.css",
     ];
     public $js = [
-        "library/js/jquery.dataTables.js",
+        "media/js/jquery.dataTables.js",
     ];
     public $depends = [
         'yii\web\JqueryAsset',
